f64bbe182c701cea5730884f00d295408c377b9f
Skylar Saveland <skylar.saveland@gmail.com>
2010-12-16 01:29:25 -0500

    .

--- listings/forms.py
+++ listings/forms.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 from django import forms
 
+from olwidget.widgets import EditableMap
+
 from listings.models import Listing
 
 class ListingForm(forms.ModelForm):
+
+    location = forms.CharField(widget=EditableMap())
 
     class Meta:
         model = Listing
