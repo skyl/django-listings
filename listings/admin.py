f33fa48525386c679d1846ed85b2f45f58f1021b
Skylar Saveland <skylar.saveland@gmail.com>
2010-12-25 23:50:56 -0500

    crucial security release ;9

--- listings/admin.py
+++ listings/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
+from olwidget.admin import GeoModelAdmin
+
 from listings.models import Listing
 
-class ListingAdmin(admin.ModelAdmin):
+
+class ListingGeoAdmin(GeoModelAdmin):
+
     list_display        = ('owner', 'title', 'description', 'want', 'state',)
     list_filter         = ('owner', 'state')
     search_fields       = ('title', 'description', 'want')
 
-admin.site.register(Listing, ListingAdmin)
+    list_map = ['location']
+    list_map_options = {
+        'cluster': True,
+        'cluster_display': 'list',
+    }
+
+    options = {
+        'layers': ['google.streets'],
+        'default_lat': 44,
+        'default_lon': -72,
+    }
+
+
+admin.site.register(Listing, ListingGeoAdmin)
 
 
--- listings/forms.py
+++ listings/forms.py
@@ -5,9 +5,11 @@ from olwidget.widgets import EditableMap
 
 from listings.models import Listing
 
+from geolistings.views import default_map_options
+
 class ListingForm(forms.ModelForm):
 
-    location = forms.CharField(widget=EditableMap())
+    location = forms.CharField(widget=EditableMap(default_map_options))
 
     class Meta:
         model = Listing
